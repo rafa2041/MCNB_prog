9a72b890a60e79f58ba27d37d97975446568d0e7
rafa2041 <rafael.avelar2010@gmail.com>
2024-12-13 10:28:07 +0100

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,313 @@
+import os, json, pickle
+import numpy as np
+import pandas as pd
+import string
+import random
+from OpenGL.GL import *
+import random
+import pygame
+
+
+class Exp():
+
+    def __init__(self, subj_ID, n_list, num_rounds, proportion_repeats, 
+    im_h=300, im_w=300, stim_interval=2, stim_duration=0.5):
+
+        upper_list = ['P', 'Q', 'A', 'M', 'Z', 'X', 'K', 'N', 'T']
+        self.num_elements = len(upper_list)
+
+        shape_list = [0, 1, 2]
+        color_list = [[255,0,0],[0,255,0], [0,0,255]]
+        
+        # basic params
+        self.subj_ID = subj_ID
+        self.num_rounds = num_rounds
+        self.proportion_repeats = proportion_repeats
+        self.n_list = n_list
+
+        #Stimuli params
+        self.shapes_and_colors = []
+        self.letters_list = upper_list
+
+        self.shapes_colors_list = []
+        for i in shape_list:
+            for j in color_list:
+                self.shapes_colors_list.append([i,j])
+        
+
+        #Stimuli lists
+        self.stim_indeces = []
+        self.stim_locs = []
+        
+        # image params
+        self.im_h = im_h
+        self.im_w = im_w
+
+        # timing params
+        self.stim_interval = stim_interval
+        self.stim_duration = stim_duration
+
+        # data collection
+        self.fullData = {}
+        self.fullData['ID'] = self.subj_ID
+        self.fullData['true_vals_letters'] = np.zeros((len(n_list), num_rounds))
+        self.fullData['responses_letters'] = np.zeros((len(n_list), num_rounds))
+        self.fullData['true_vals_shapes'] = np.zeros((len(n_list), num_rounds))
+        self.fullData['responses_shapes'] = np.zeros((len(n_list), num_rounds))
+
+    #From Aoran: create the list of stimuli
+    def makeNback(self, k, n_trials, n_back_level, target_proportion):
+        stim_sequence = random.choices(range(k), k=n_trials)  # 30 random numbers between 0-17
+        n_targets = int(target_proportion * n_trials)  # number of target trials 
+        
+        # first count natural matches
+        natural_targets = []
+        for i in range(n_back_level, n_trials):
+            if stim_sequence[i] % 9 == stim_sequence[i - n_back_level] % 9:  # check only the "letter" part, ignoring case
+                natural_targets.append(i)
+        
+        # truncate natural matches if >20% of total trials
+        if len(natural_targets) > n_targets:
+            excess_targets = len(natural_targets) - n_targets
+            extra_targets = random.sample(natural_targets, excess_targets)  # randomly remove extras
+            for target_index in extra_targets:
+                stim_sequence[target_index] = random.choice([x for x in range(18) if x % 9 != stim_sequence[target_index - n_back_level] % 9])  # replace with non-matching number
+        
+        # recheck natural matches 
+        natural_targets = [i for i in range(n_back_level, n_trials) if stim_sequence[i] % 9 == stim_sequence[i - n_back_level] % 9]
+        
+        # add missing targets if natural matches are not enough
+        total_targets = len(natural_targets)  # number of targets present
+        if total_targets < n_targets:
+            missing_targets = n_targets - total_targets  # calculate # of targets are missing
+            available_positions = [i for i in range(n_back_level, n_trials) if i not in natural_targets]
+            force_positions = random.sample(available_positions, missing_targets)  # force targets in these positions
+            
+            for target_index in force_positions:
+                # ensuring N-back logic 
+                stim_sequence[target_index] = stim_sequence[target_index - n_back_level]  
+                natural_targets.append(target_index)  # add to the list of targets
+        
+        # final check 
+        actual_targets = [i for i in range(n_back_level, n_trials) if stim_sequence[i] % 9 == stim_sequence[i - n_back_level] % 9]
+
+        # if there are still too many, remove extra targets
+        if len(actual_targets) > n_targets:
+            excess_targets = len(actual_targets) - n_targets
+            extra_targets = random.sample(actual_targets, excess_targets)
+            for target_index in extra_targets:
+                stim_sequence[target_index] = random.choice([x for x in range(18) if x % 9 != stim_sequence[target_index - n_back_level] % 9])
+
+        # recheck to ensure exactly 6 targets exist
+        #In case we couldn't remove it, take it into account
+        repeatLocs = np.zeros((n_trials))
+        repeatLocsEnd = np.zeros((n_trials))
+        for i in range(n_trials-n_back_level):
+            repeatLocs[i] = stim_sequence[i+n_back_level] == stim_sequence[i]
+        repeatLocsEnd[n_back_level:n_trials] = repeatLocs[0:n_trials-n_back_level]
+
+        return stim_sequence, repeatLocsEnd
+
+    def makeNback2(self, elements, size, n, p):
+        '''
+        INPUT PARAMETERS
+        elements: any python list
+        size: length of the output list
+        n: value of the n-back task
+        p: proportion of stimuli which will be repeated in n-back fashion
+
+        OUTPUTS
+        result: list of length size containing
+                stimuli selected from elements and arranged according to n-back task
+        repeatLocs: list of length size. Equals 1 at locations where a repetition will occur
+        '''
+        numRepeats = round(p*size)
+
+        result = []
+        # Ensure that there are no unintended repetitions
+        for i in range(size):
+            available_elems = [elem for elem in elements if i < n or result[i - n] != elements]
+            if not available_elems:
+                raise ValueError("Not enough elements to satisfy the non-repeating condition.")
+            result.append(random.choice(available_elems))
+
+        #Randomly generate the locations which will have repeated elements
+        repeatLocs = np.zeros((size))
+        repeatLocsEnd = np.zeros((size))
+        indeces = np.array(random.sample(range(size-n), numRepeats))
+        repeatLocs[random.sample(range(size-n), numRepeats)] = 1
+        repeatLocsEnd[n:size] = repeatLocs[0:size-n]
+
+        result = np.array(result)
+
+        for i in range(size-n):
+            if repeatLocs[i]: 
+                result[i+n] = result[i]
+
+        #In case an additional repetition was added in the process, try to remove them
+        for i in range(size-n):
+            if result[i+n] == result[i] and not(repeatLocs[i] or repeatLocsEnd[i]):
+                result[i] = elements[random.randint(0,len(elements)-1)]
+
+        #In case we couldn't remove it, take it into account
+        for i in range(size-n):
+            repeatLocs[i] = result[i+n] == result[i]
+            
+
+        repeatLocsEnd[n:size] = repeatLocs[0:size-n]
+        
+        return result.tolist(), repeatLocsEnd.tolist()
+
+    def generate_trials(self, n):
+        self.stim_indeces, self.stim_locs = self.makeNback(self.num_elements, self.num_rounds, n, self.proportion_repeats)
+
+    def save_expt(self):
+        # this is how to save a pandas dict to csv
+
+        newpath = f'./exported_data/ID_{self.subj_ID}'
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+ 
+        true_vals_letters_frame = pd.DataFrame(self.fullData['true_vals_letters'])
+        responses_letters_frame = pd.DataFrame(self.fullData['responses_letters'])
+        true_vals_shapes_frame = pd.DataFrame(self.fullData['true_vals_shapes'])
+        responses_shapes_frame = pd.DataFrame(self.fullData['responses_shapes'])
+
+        true_vals_letters_frame.to_csv(f'./exported_data/ID_{self.subj_ID}/true_vals_letters', index=False)
+        responses_letters_frame.to_csv(f'./exported_data/ID_{self.subj_ID}/responses_letters', index=False)
+        true_vals_shapes_frame.to_csv(f'./exported_data/ID_{self.subj_ID}/true_vals_shapes', index=False)
+        responses_shapes_frame.to_csv(f'./exported_data/ID_{self.subj_ID}/responses_shapes', index=False)
+    
+
+class RectangleCustom:
+    def __init__(self, center, width, height, orientation=0.0, fill=True, color=(255.0, 255.0, 255.0)):
+        """
+        Initialize a Rectangle object.
+
+        Parameters:
+            center: Center coordinates of the rectangle as a tuple (x, y).
+            width: Width of the rectangle.
+            height: Height of the rectangle.
+            orientation: Rotation angle of the rectangle in degrees.
+            fill: If True, the rectangle will be filled. If False, only the outline will be drawn.
+            color: RGB tuple (r, g, b) for the rectangle's color, with values between 0 and 255.
+        """
+        self.center = np.asarray(center, dtype=np.float32)
+        self.width = float(width)
+        self.height = float(height)
+        self.orientation = float(orientation)
+        self.fill = fill
+        self.set_color(color)
+
+    def set_center(self, center):
+        self.center = np.asarray(center, dtype=np.float32)
+
+    def set_size(self, width, height):
+        self.width = float(width)
+        self.height = float(height)
+
+    def set_orientation(self, orientation):
+        self.orientation = float(orientation)
+
+    def set_color(self, color):
+        self.color = np.asarray(color) / 255.0  # Normalize color to [0, 1]
+
+    def draw(self):
+        """
+        Draw the rectangle using OpenGL.
+        """
+        glColor3f(*self.color)  # Set the rectangle's color
+
+        # Choose the drawing mode
+        if self.fill:
+            glBegin(GL_QUADS)
+        else:
+            glBegin(GL_LINE_LOOP)
+
+        # Calculate vertices of the rectangle
+        w, h = self.width / 2, self.height / 2
+        vertices = np.array([
+            [-w, -h],
+            [ w, -h],
+            [ w,  h],
+            [-w,  h],
+        ], dtype=np.float32)
+
+        # Rotate vertices based on the orientation
+        angle_rad = np.radians(self.orientation)
+        rotation_matrix = np.array([
+            [np.cos(angle_rad), -np.sin(angle_rad)],
+            [np.sin(angle_rad),  np.cos(angle_rad)]
+        ], dtype=np.float32)
+        rotated_vertices = np.dot(vertices, rotation_matrix.T)
+
+        # Translate vertices to the center
+        for vertex in rotated_vertices:
+            glVertex2f(self.center[0] + vertex[0], self.center[1] + vertex[1])
+
+        glEnd()
+
+
+class EquilateralTriangle:
+    def __init__(self, center, side_length, orientation=0.0, color=(255, 255, 255), fill=True):
+        """
+        Initialize an EquilateralTriangle object.
+
+        Parameters:
+            center: Center coordinates of the triangle as a tuple (x, y).
+            side_length: Length of each side of the triangle.
+            orientation: Rotation angle of the triangle in degrees.
+            color: RGB tuple (r, g, b) for the triangle's color, with values between 0 and 255.
+            fill: If True, the triangle will be filled. If False, only the outline will be drawn.
+        """
+        self.center = np.array(center, dtype=np.float32)
+        self.side_length = float(side_length)
+        self.orientation = float(orientation)
+        self.set_color(color)
+        self.fill = fill
+
+    def set_color(self, color):
+        self.color = np.array(color) / 255.0  # Normalize color to [0, 1]
+
+    def draw(self):
+        """
+        Draw the equilateral triangle using OpenGL.
+        """
+        # Calculate the vertices of the triangle
+        half_height = (np.sqrt(3) / 2) * self.side_length / 2
+        vertices = np.array([
+            [0, 2 * half_height],  # Top vertex
+            [-self.side_length / 2, -half_height],  # Bottom-left vertex
+            [self.side_length / 2, -half_height]   # Bottom-right vertex
+        ])
+
+        glPushMatrix()  # Save the current transformation matrix
+
+        # Apply transformations
+        glTranslatef(self.center[0], self.center[1], 0)
+        glRotatef(self.orientation, 0, 0, 1)
+
+        # Set the color
+        glColor3f(*self.color)
+
+        # Draw the triangle
+        if self.fill:
+            glBegin(GL_TRIANGLES)
+        else:
+            glBegin(GL_LINE_LOOP)
+        
+        for vertex in vertices:
+            glVertex2f(vertex[0], vertex[1])
+        glEnd()
+
+        glPopMatrix()  # Restore the transformation matrix
+
+
+
+def drawPracticeRectangle(space_pressed, stim_correct, CorrectRectangle):
+    if space_pressed and stim_correct:
+        CorrectRectangle.set_color([0,255,0])
+        CorrectRectangle.draw()
+    if space_pressed and not stim_correct:
+        CorrectRectangle.set_color([255,0,0])
+        CorrectRectangle.draw()
---
+++ player.py
@@ -0,0 +1,258 @@
+'''
+NOTE: this code is based on and extends the code from tachypy_example3.py.
+'''
+import time
+from tachypy import (
+    Text,
+    Screen,
+    FixationCross,
+    center_rect_on_point,
+    ResponseHandler,
+    Rectangle,
+)
+
+### INITIALIZE THE EXPERIMENT (THE IDEA IS THAT ONCE THE CODE RUNS YOU WILL ONLY EVER CHANGE THIS LINE
+### TO CHANGE THE SUBJECT ID, EXPERIMENT PARAMETERS, ETC)
+from generator import *
+exp = Exp(subj_ID = 3, n_list = [1,2], num_rounds = 10, proportion_repeats = 0.22, 
+    im_h=300, im_w=300, stim_interval=2, stim_duration=0.5)
+practice = 1
+
+#Note: We compensate for algorithm by using p = 0.22 instead of 0.2
+
+### CONVERT S TO NS FOR TIMING
+stim_interval_ns = exp.stim_interval*1e9
+stim_duration_ns = exp.stim_duration*1e9
+
+### SETUP STUFF THAT WE'LL USE FOR ALL SCRIPTS
+# Define screen we will draw to
+screen_number = 1
+screen = Screen(screen_number=screen_number, fullscreen=False, desired_refresh_rate=60)
+
+# flip the screen (needed for frame_rate measruement below)
+screen.fill([128, 128, 128])
+# flip the screen to make the background color visible
+screen.flip()
+
+# get some relevant screen properties
+center_x = screen.width//2
+center_y = screen.height//2
+frame_rate_measured = 1/screen.test_flip_intervals(num_frames=100)
+half_ifi_s = 1/(2*frame_rate_measured)  # half the inter-frame interval in seconds
+half_ifi_ns = half_ifi_s*1e9  # half the inter-frame interval in nanoseconds
+print(f'Measured frame rate: {frame_rate_measured:.2f} Hz')
+
+rectangle = RectangleCustom([center_x, center_y], 400, 200)
+square = RectangleCustom([center_x, center_y], 250, 250)
+triangle = EquilateralTriangle([center_x, center_y], 300, orientation = 180)
+
+shape_list = [rectangle, square, triangle]
+
+CorrectRectangle = RectangleCustom([center_x, center_y-300], 100, 100)
+
+
+### LOAD UP STUFF WE WANT TO SHOW ON SCREEN
+fixation_cross = FixationCross(center=[center_x, center_y], half_width=10, half_height=10, thickness=5.0, color=(0, 0, 0))
+
+# define the position in which the Texture will be mapped.
+dest_rect = center_rect_on_point([0, 0, exp.im_w-1, exp.im_h-1], [center_x, center_y])
+
+### INITIALIZE RESPONSE HANDLER
+response_handler = ResponseHandler(keys_to_listen=['escape', 'space'])
+
+# flip an initial screen and record starting time
+screen.fill([128, 128, 128])
+start_time = screen.flip() # returns time in ns
+#exp.logs_timing['start'] = start_time/1e9 # ns to s
+
+
+### PRESENT WELCOME TEXT AND WAIT FOR RESPONSE TO START
+# print welcome message
+screen.fill([128, 128, 128])
+welcome_message = Text('Welcome to the experiment! Press the space bar to proceed, escape to quit', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+welcome_message.draw()
+screen.flip()
+while True:
+    response_handler.get_events()
+    if response_handler.is_key_down('space'):
+        break
+
+variation_list = ['letter', 'shape']
+if(exp.subj_ID%2==0): variation_list = ['shape', 'letter']
+exp.fullData['variation_list'] = variation_list
+
+exp.save_expt()
+
+# ### LOOP OVER TRIALS
+for variation in variation_list: #Loop over letters and shapes
+
+    for j in range(len(exp.n_list)): #Loop over n = 1,2,3
+        n = exp.n_list[j]
+        shape_message = Text(f'Shapes will be presented one at a time. Take note of them. If the shape on the screen is the same type and color as the one seen {n} letters before, press the spacebar. Press the spacebar to proceed.', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+        letter_message = Text(f'Letters will be presented one at a time. Take note of them. If the letter on the screen is the same as the one seen {n} letters before, press the spacebar. The case of the letter does not matter. Press the spacebar to proceed.', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+        if(variation == 'letter'): message = letter_message
+        if(variation == 'shape'): message = shape_message
+        screen.fill([128, 128, 128])
+        message.draw()
+        screen.flip()
+        while True:
+            response_handler.get_events()
+            if response_handler.is_key_down('space'):
+                break
+
+        trial_start_ns = time.monotonic_ns() # get the current time in nanoseconds
+
+        # fixation cross
+        estimated_time = trial_start_ns + stim_interval_ns
+        while time.monotonic_ns() < estimated_time - half_ifi_ns:
+            screen.fill([128, 128, 128])
+            fixation_cross.draw()
+            true_time = screen.flip()
+
+        exp.generate_trials(n)
+
+        exp.fullData[f'true_vals_{variation}s'][j,:] = exp.stim_locs
+
+        for round in range(exp.num_rounds): #Gp through 50 stimuli presentations
+
+            space_pressed = 0
+            # stimulus presentation
+            if(variation == 'letter'):
+                letter = exp.letters_list[exp.stim_indeces[round]]
+                if((int)(random.randrange(0, 2)) & 1): letter = letter.lower()
+                display = Text(letter, dest_rect=dest_rect, font_name='Helvetica', font_size=100, color=(0, 0, 0))
+            else:
+                shape = exp.shapes_colors_list[exp.stim_indeces[round]][0]
+                color = exp.shapes_colors_list[exp.stim_indeces[round]][1]
+                display = shape_list[shape]
+                display.set_color(color)
+
+            estimated_time = estimated_time + stim_duration_ns
+            while time.monotonic_ns() < estimated_time - half_ifi_ns:
+                screen.fill([128, 128, 128])
+                display.draw()
+                if practice: drawPracticeRectangle(space_pressed, exp.stim_locs[round], CorrectRectangle)
+
+                true_time = screen.flip()
+
+                response_handler.get_events()
+                if response_handler.is_key_down('space') and not space_pressed:
+                    space_pressed = 1
+
+
+            #inter-stimulus interval
+            estimated_time = estimated_time + stim_interval_ns
+            while time.monotonic_ns() < estimated_time - half_ifi_ns:
+                screen.fill([128, 128, 128])
+                fixation_cross.draw()
+                if practice: drawPracticeRectangle(space_pressed, exp.stim_locs[round], CorrectRectangle)
+                
+                response_handler.get_events()
+                if response_handler.is_key_down('space') and not space_pressed:
+                    space_pressed = 1
+
+                true_time = screen.flip()
+
+            exp.fullData[f'responses_{variation}s'][j,round] = space_pressed
+                
+            
+screen.fill([128, 128, 128])
+goodbye_message = Text('Experiment finished! Press the space bar to quit', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+goodbye_message.draw()
+screen.flip()
+while True:
+    response_handler.get_events()
+    if response_handler.is_key_down('space'):
+        break 
+            
+
+
+
+
+# for trial in num_trials
+#     # # create empty "subdicts" for this trial (only needed for dicts, not for pandas)
+#     # exp.logs_timing[f'trial_{trial}'] = {}
+
+#     # # log the start of the trial and start trial timer
+#     trial_start_ns = time.monotonic_ns() # get the current time in nanoseconds
+#     # exp.logs_timing[f'trial_{trial}']['trialStart'] = trial_start_ns/1e9  # log in seconds
+
+#     # # look into our trial list to get the categ and image id
+#     # trial_categ = exp.trial_info.loc[trial, 'categ']
+#     # trial_categ_id = exp.trial_info.loc[trial, 'categ_id']
+#     # trial_img = exp.trial_info.loc[trial, 'img_id']
+#     # # get the corresponding texture
+#     # trial_texture = all_textures[trial_categ][trial_img]
+        
+
+#     # pre-stimulus blank screen presented for trial_start_interval_ns nanoseconds
+#     estimated_time = trial_start_ns + stim_interval_ns
+#     while time.monotonic_ns() < estimated_time - half_ifi_ns:
+#         screen.fill([128, 128, 128])
+#         fixation_cross.draw()
+#         true_time = screen.flip()
+#     exp.logs_timing[f'trial_{trial}']['stimOnset'] = {'estimated': (estimated_time-trial_start_ns)/1e9, 
+#                                                       'true': (true_time-trial_start_ns)/1e9} # ns to s
+#     # stimulus presentation
+#     estimated_time = estimated_time + stim_duration_ns
+#     while time.monotonic_ns() < estimated_time - half_ifi_ns:
+#         screen.fill([128, 128, 128])
+#         trial_texture.draw(dest_rect)
+#         fixation_cross.draw()
+#         true_time = screen.flip()
+#     exp.logs_timing[f'trial_{trial}']['stimOffset'] = {'estimated': (estimated_time-trial_start_ns)/1e9, 
+#                                                        'true': (true_time-trial_start_ns)/1e9} # ns to s
+
+
+#     # get response
+#     screen.fill([128, 128, 128])
+#     instruction = Text('Press left arrow for cameleon, right for goat', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+#     instruction.draw()
+#     response_start_time = screen.flip()
+#     response_handler.get_events() # get rid of any lingering key presses
+#     while True:
+#         response_handler.get_events()
+#         if response_handler.is_key_down('left'):
+#             response = 0
+#             break
+#         elif response_handler.is_key_down('right'):
+#             response = 1
+#             break
+#         elif response_handler.is_key_down('escape'):
+#             screen.close()
+#             exit()
+#     response_given_time = time.monotonic_ns()
+#     exp.trial_responses.loc[trial, 'response'] = response
+#     exp.trial_responses.loc[trial, 'RT'] = (response_given_time - response_start_time)/1e9  # ns to s
+#     correct = response == trial_categ_id
+#     exp.trial_responses.loc[trial, 'accuracy'] = correct
+    
+#     # wait with sloppy timing as this is not critical
+#     time.sleep(0.5)
+
+#     # Display feedback text
+#     screen.fill([128, 128, 128])
+#     text_str = 'Correct!' if correct else 'Incorrect.'
+#     feedback_message = Text(text_str, dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+#     feedback_message.draw()
+#     screen.flip()
+
+#     # wait with sloppy timing as this is not critical
+#     time.sleep(0.5)
+
+# # print goodbye message
+# screen.fill([128, 128, 128])
+# goodbye_message = Text('Goodbye!', dest_rect=dest_rect, font_name='Helvetica', font_size=24, color=(0, 0, 0))
+# goodbye_message.draw()
+# screen.flip()
+
+#  # wait with sloppy timing as this is not critical
+# time.sleep(2)
+
+# # close the screen
+# screen.close()
+
+# # save everything (responses, timing_logs, and Exp instance)
+
+exp.save_expt()
+print('Experiment finished!')
